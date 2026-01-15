9a15721f7216c22c095adbdddf9a4517a400d67e
LesFerch <79026235+LesFerch@users.noreply.github.com>
2026-01-14 20:52:42 -0500

    Add files via upload

--- DesktopPicViewer/Properties/AssemblyInfo.cs
+++ DesktopPicViewer/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.0.2.0")]
+[assembly: AssemblyFileVersion("2.0.2.0")]
